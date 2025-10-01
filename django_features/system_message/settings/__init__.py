aac3a98be4bb7f6efc5146b37ca93d05f849a011
Joel-Luca <joel.christinet@gmx.ch>
2025-10-01 07:04:28 +0200

    Set env variables as default for the system message feature
    Therefore, the default can be overwritten per app.

--- django_features/system_message/settings/__init__.py
+++ django_features/system_message/settings/__init__.py
@@ -1,13 +1,23 @@
+from configurations import values
+
+
 class SystemMessageConfigurationMixin:
+    SYSTEM_MESSAGE_PERMISSION = values.Value("")
+    ENABLE_SYSTEM_MESSAGE = values.BooleanValue(False)
+
     @property
     def CONSTANCE_CONFIG(self) -> dict:
         return {
             "SYSTEM_MESSAGE_PERMISSION": (
-                "",
+                self.SYSTEM_MESSAGE_PERMISSION,
                 "Django permission to manage system messages.",
                 str,
             ),
-            "ENABLE_SYSTEM_MESSAGE": (False, "Enables the system info feature.", bool),
+            "ENABLE_SYSTEM_MESSAGE": (
+                self.ENABLE_SYSTEM_MESSAGE,
+                "Enables the system info feature.",
+                bool,
+            ),
         }
 
     @property
