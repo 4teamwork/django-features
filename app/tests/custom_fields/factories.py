b77148ab8e6b4cb04485cf77ab11c246ddc800c5
Joel-Luca <joel.christinet@gmx.ch>
2025-10-01 09:35:26 +0200

    Add custom field factories for testing

---
+++ app/tests/custom_fields/factories.py
@@ -0,0 +1,21 @@
+from factory import SubFactory  # type: ignore
+
+from app.tests.system_message.factories import BaseFactory  # type: ignore
+from django_features.custom_fields import models
+
+
+class CustomFieldFactory(BaseFactory):
+    class Meta:
+        model = models.CustomField
+
+    field_type = models.CustomField.FIELD_TYPES.CHAR
+    identifier = "custom_field"
+    label = "Custom Field Label"
+
+
+class CustomValueFactory(BaseFactory):
+    class Meta:
+        model = models.CustomValue
+
+    field = SubFactory(CustomFieldFactory)  # type: ignore
+    value = "custom value"
