1af080c1676296042b05f25a4c9e78e0f638a254
Joel-Luca <joel.christinet@gmx.ch>
2025-10-01 09:37:30 +0200

    Add mapping field

--- django_features/settings/fields.py
+++ django_features/settings/fields.py
@@ -3,6 +3,8 @@ from typing import Any
 
 from django.forms import fields
 
+from django_features.validations import MappingValidationMixin
+
 
 class PrettyJSONEncoder(json.JSONEncoder):
     def __init__(self, *args: Any, indent: int, sort_keys: bool, **kwargs: Any) -> None:
@@ -13,3 +15,14 @@ class PrettyJSONField(fields.JSONField):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         kwargs["encoder"] = PrettyJSONEncoder
         super().__init__(*args, **kwargs)
+
+
+class ModelFieldMapping(MappingValidationMixin, PrettyJSONField):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    def validate(self, value: Any) -> None:
+        super().validate(value)
+        if value is None:
+            return
+        self.validate_models_field_mapping(value)
