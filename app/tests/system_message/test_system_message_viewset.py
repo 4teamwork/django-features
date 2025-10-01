e0cfcda3638db37ecabde62ebd639b907efc1ea7
Joel-Luca <joel.christinet@gmx.ch>
2025-10-01 07:04:51 +0200

    Add test for the permissions of the system messages
    Move system message tests to a subfolder for a better overview.

--- app/tests/system_message/test_system_message_viewset.py
+++ app/tests/system_message/test_system_message_viewset.py
@@ -2,11 +2,13 @@ import datetime
 
 import pytz
 from constance.test import override_config
+from django.contrib.auth.models import Permission
 from freezegun import freeze_time
 from pluck import pluck
 
 from app.tests import APITestCase
-from app.tests.factories.factories import SystemMessageFactory
+from app.tests.system_message.factories import SystemMessageFactory
+from app.tests.system_message.factories import SystemMessageTypeFactory
 
 
 class SystemInfoViewSetTest(APITestCase):
@@ -62,3 +64,34 @@ class SystemInfoViewSetTest(APITestCase):
         data = response.json()
         self.assertEqual(1, len(data))
         self.assertEqual([info_1.title], pluck(data, "title"))
+
+    @override_config(
+        ENABLE_SYSTEM_MESSAGE=True,
+        SYSTEM_MESSAGE_PERMISSION="system_message.change_systemmessage",
+    )
+    def test_system_message_viewset_permission(self) -> None:
+        system_message_type = SystemMessageTypeFactory()
+
+        data = {
+            "begin": "2025-10-01T04:50:49.592Z",
+            "text_color": "#FFFFFF",
+            "background_color": "#0000FF",
+            "title": "Hello World",
+            "type_id": system_message_type.id,
+            "text": "Test",
+        }
+
+        self.assertFalse(self.user.has_perm("system_message.change_systemmessage"))
+
+        response = self.client.get("/api/system_message")
+        self.assertEqual(200, response.status_code)
+
+        response = self.client.post("/api/system_message", data)
+        self.assertEqual(403, response.status_code)
+
+        self.user.user_permissions.add(
+            Permission.objects.get(codename="change_systemmessage")
+        )
+
+        response = self.client.post("/api/system_message", data)
+        self.assertEqual(201, response.status_code)
