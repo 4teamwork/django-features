bc6ca124223598769f885cbf80879ea9e176c888
Joel-Luca <joel.christinet@gmx.ch>
2025-10-21 09:04:20 +0200

    Fix choice serializer value fields

--- django_features/custom_fields/fields.py
+++ django_features/custom_fields/fields.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from django.core.exceptions import ObjectDoesNotExist
+from pluck import pluck
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.utils.model_meta import get_field_info
@@ -34,30 +35,44 @@ class ChoiceIdField(serializers.Field):
     def get_queryset(self) -> CustomValueQuerySet:
         return CustomValue.objects.filter(field_id=self.field.id)
 
-    def to_internal_value(self, data: Any) -> CustomValue | CustomValueQuerySet:
-        qs = self.get_queryset()
-        if isinstance(data, int) or isinstance(data, str):
-            try:
-                return qs.get(**{self.unique_field: data})
-            except ObjectDoesNotExist:
-                raise ValidationError(
-                    f"Custom value with the {self.unique_field} {data} does not exist."
-                )
-        elif isinstance(data, list):
-            values = qs.filter(**{f"{self.unique_field}__in": data})
-            if values.count() != len(data):
-                missing = set(data) - set(
-                    values.values_list(self.unique_field, flat=True)
-                )
-                raise ValidationError(
-                    f"Some of the given {self.unique_field}s do not match: {missing}"
-                )
-            return values
-        raise ValidationError(
-            f"The given value {data} has not a valid type. Expected a list or int."
-        )
     def to_representation(
         self, value: CustomValue | CustomValueQuerySet
     ) -> int | list[int]:
         return CustomChoiceSerializer(value, many=self.field.multiple_choice).data
 
+    def _choice_field(self, data: int | str | dict) -> CustomValue:
+        if isinstance(data, dict):
+            value = data.get("id")
+        else:
+            value = data
+        try:
+            return self.get_queryset().get(**{self._unique_field: value})
+        except ObjectDoesNotExist:
+            raise ValidationError(
+                f"Custom value with the {self._unique_field} {data} does not exist."
+            )
+
+    def _multiple_choice(self, data: list[int | str | dict]) -> CustomValueQuerySet:
+        if all(type(d) is dict for d in data):
+            value = pluck(data, self._unique_field)
+        else:
+            value = data
+        values = self.get_queryset().filter(**{f"{self._unique_field}__in": value})
+        if values.count() != len(data):
+            missing = set(data) - set(values.values_list(self._unique_field, flat=True))
+            raise ValidationError(
+                f"Some of the given {self._unique_field}s do not match: {missing}"
+            )
+        return values
+
+    def to_internal_value(self, data: Any) -> CustomValue | CustomValueQuerySet:
+        if self.field.multiple_choice and isinstance(data, list):
+            return self._multiple_choice(data)
+        elif self.field.choice_field and isinstance(data, (int, str, dict)):
+            return self._choice_field(data)
+        else:
+            if not self.field.choice_field:
+                raise ValidationError(f"The field {self.field} is not a choice field.")
+            raise ValidationError(
+                f"The given value {data} has not a valid type. Expected a list or int."
+            )
