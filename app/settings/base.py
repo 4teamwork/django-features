55fab0c886af9366eed0dea6edb6b6e5d90cb57c
Joel-Luca <joel.christinet@gmx.ch>
2026-02-25 08:00:39 +0100

    Define custom model settings for dummy app

--- app/settings/base.py
+++ app/settings/base.py
@@ -161,3 +161,6 @@ class Base(BaseConfiguration, SystemMessageConfigurationMixin):
         }
 
     SECRET_KEY = values.SecretValue()
+
+    CUSTOM_FIELD_MODEL = values.Value("custom_field.CustomField")
+    CUSTOM_FIELD_VALUE_MODEL = values.Value("custom_field.CustomValue")
