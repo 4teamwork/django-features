6484bc4d34ba623daf24d0473de2126e26f75d68
Joel-Luca <joel.christinet@gmx.ch>
2026-02-25 07:58:21 +0100

    Inherit from base models in dummy app

---
+++ app/custom_field/models/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["CustomField", "CustomValue"]
+
+
+from .field import CustomField
+from .value import CustomValue
---
+++ app/custom_field/models/base.py
@@ -0,0 +1,30 @@
+from django.contrib.contenttypes.fields import GenericRelation
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from app.custom_field.models import CustomField
+from app.custom_field.models import CustomValue
+from django_features.custom_fields.models import CustomFieldBaseModel
+from django_features.custom_fields.models import CustomFieldTypeBaseModel
+
+
+class CustomTypeBaseModel(CustomFieldTypeBaseModel):
+    custom_fields = GenericRelation(
+        CustomField,
+        object_id_field="type_id",
+        content_type_field="type_content_type",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class CustomBaseModel(CustomFieldBaseModel):
+    custom_values = models.ManyToManyField(
+        blank=True,
+        to=CustomValue,
+        verbose_name=_("Benutzerdefinierte Werte"),
+    )
+
+    class Meta:
+        abstract = True
