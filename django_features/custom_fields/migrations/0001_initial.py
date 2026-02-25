0a4d0100d49a73b6b5c8036cb720cbcf49474945
Joel-Luca <joel.christinet@gmx.ch>
2026-02-25 07:51:28 +0100

    Remove swappable model dependencies

--- django_features/custom_fields/migrations/0001_initial.py
+++ django_features/custom_fields/migrations/0001_initial.py
@@ -2,20 +2,15 @@
 
 import django.db.models.deletion
 import django_extensions.db.fields
-from django.conf import settings
 from django.db import migrations
 from django.db import models
-from django.db.migrations import swappable_dependency
 
 
 class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ("contenttypes", "0002_remove_content_type_name"),
-        swappable_dependency(settings.CUSTOM_FIELD_MODEL),
-    ]
+    dependencies = [("contenttypes", "0002_remove_content_type_name")]
 
     operations = [
         migrations.CreateModel(
@@ -160,7 +155,7 @@ class Migration(migrations.Migration):
                     models.ForeignKey(
                         on_delete=django.db.models.deletion.CASCADE,
                         related_name="values",
-                        to=settings.CUSTOM_FIELD_MODEL,
+                        to="custom_fields.customfield",
                         verbose_name="Feld",
                     ),
                 ),
