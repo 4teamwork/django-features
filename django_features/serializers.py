732aa5b77ea2de6bf6346d0e69f61cee166fb782
Joel-Luca <joel.christinet@gmx.ch>
2026-08-14 07:58:02 +0200

    List mappings now apply the default and formatter hooks to each item.

--- django_features/serializers.py
+++ django_features/serializers.py
@@ -131,10 +131,13 @@ class BaseMappingSerializer(CustomFieldBaseModelSerializer, PropertySerializerMi
                         ),
                         required=False,
                     )
+        initial_data = getattr(self, "initial_data", {})
+        if not isinstance(initial_data, dict):
+            initial_data = {}
         for field_name, field in nested_fields.items():
-            nested_data = self.initial_data.get(field_name)
+            nested_data = initial_data.get(field_name, empty)
             if not isinstance(nested_data, dict):
-                continue
+                nested_data = empty
             self.related_fields.add(field_name)
             fields[field_name] = NestedMappingSerializer(
                 data=nested_data,
@@ -154,11 +157,12 @@ class BaseMappingSerializer(CustomFieldBaseModelSerializer, PropertySerializerMi
     def create(self, validated_data: dict[str, Any]) -> models.Model:
         relations_to_save: dict[str, Any] = {}
         for field in self.related_fields:
-            value = validated_data.pop(field, None)
+            if field not in validated_data:
+                continue
+            value = validated_data.pop(field)
             serializer = self.fields.get(field)
             if isinstance(serializer, NestedMappingSerializer):
-                serializer.is_valid(raise_exception=True)
-                relations_to_save[field] = serializer.save()
+                relations_to_save[field] = serializer.create(value)
             elif value is not None:
                 relations_to_save[field] = value
         instance = super().create(validated_data)
@@ -175,11 +179,12 @@ class BaseMappingSerializer(CustomFieldBaseModelSerializer, PropertySerializerMi
         self, instance: models.Model, validated_data: dict[str, Any]
     ) -> models.Model:
         for field in self.related_fields:
-            value = validated_data.pop(field, None)
+            if field not in validated_data:
+                continue
+            value = validated_data.pop(field)
             serializer = self.fields.get(field)
             if isinstance(serializer, NestedMappingSerializer):
-                serializer.is_valid(raise_exception=True)
-                value = serializer.save()
+                value = serializer.create(value)
             model_field = self.model._meta.get_field(field)
             if model_field.many_to_many or model_field.one_to_many:
                 getattr(instance, field).set(value)
@@ -205,7 +210,11 @@ class NestedMappingSerializer(BaseMappingSerializer):
         self.exclude = exclude
         self.mapping_fields = nested_fields
         self.mapping = parent_mapping
-        self.Meta.model = field.related_model
+
+        class Meta(self.Meta):  # type: ignore[name-defined]
+            model = field.related_model
+
+        setattr(self, "Meta", Meta)
         super().__init__(*args, **kwargs)
 
 
@@ -236,6 +245,9 @@ class DataMappingSerializerMixin(PropertySerializerMixin):
         return {field_name: value}
 
     def map_data(self, initial_data: Any) -> Any:
+        if not isinstance(initial_data, dict):
+            return initial_data
+
         data: dict[str, Any] = {}
         for external_name, internal_name in self.model_mapping.items():
             external_field_path = external_name.split(self.relation_separator)
@@ -264,14 +276,17 @@ class ListDataMappingSerializer(serializers.ListSerializer, DataMappingSerialize
         self.mapping = kwargs.pop("mapping", {})
         self.model = kwargs.pop("model")
         self.unmapped_data = data if data is not empty else []
-        mapped_data = self.map_list_data(self.unmapped_data)
-        super().__init__(data=mapped_data, *args, **kwargs)
+        super().__init__(data=data, *args, **kwargs)
+        if data is not empty:
+            self.initial_data = self.map_list_data(data)
 
-    def map_list_data(self, initial_data: Any) -> list[Any]:
-        list_data: list[dict[str, Any]] = []
-        for item in initial_data:
-            list_data.append(self.map_data(item))
-        return list_data
+    def map_data(self, initial_data: Any) -> Any:
+        return self.child.map_data(initial_data)
+
+    def map_list_data(self, initial_data: Any) -> Any:
+        if not isinstance(initial_data, list):
+            return initial_data
+        return [self.map_data(item) for item in initial_data]
 
 
 class MappingSerializer(BaseMappingSerializer, DataMappingSerializerMixin):
